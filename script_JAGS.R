6b5e525e240f8d42ff46f595ce3ae00d2190a00b
federigia <federigia@gmail.com>
2016-12-02 16:37:01 +0100

    Script to run a Bayesian model using JAGS

--- script_JAGS.R
+++ script_JAGS.R
@@ -26,3 +26,31 @@ autocorr.plot(output)
 pairs(as.matrix(output))
 crosscorr.plot(output)
 ######################################
+
+data=list(X=X,n=n,p=p)
+init=list(mu=2,tau=1)
+modelstring="
+  model {
+    for (j in 1:p) {
+      for (i in 1:n) {
+        X[i,j]~dnorm(mu+theta[j],tau)
+      }
+    }
+    theta[1]<-0
+    for (j in 2:p) {
+      theta[j]~dnorm(0,0.0001)
+    }
+    mu~dnorm(0,0.0001)
+    tau~dgamma(1,0.0001)
+  }
+"
+model=jags.model(textConnection(modelstring),data=data,inits=init)
+update(model,n.iter=1000)
+output=coda.samples(model=model,variable.names=c("mu","tau","theta"),n.iter=100000,thin=10)
+print(summary(output))
+plot(output)
+autocorr.plot(output)
+pairs(as.matrix(output))
+crosscorr.plot(output)
+
+#############################################################################
